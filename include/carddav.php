89dd23a5a6535aa780c45beacbb1894c312c4f5c
Julien Ammous <schmurfy@gmail.com>
2013-02-12 14:37:58 +0100

    change user agent to identify itself as Z-Push
    this is not a shame...

--- include/carddav.php
+++ include/carddav.php
@@ -111,7 +111,8 @@ class carddav_backend
 	 *
 	 * @constant	string
 	 */
-	const USERAGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:13.0) Gecko/20100101 Firefox/13.0';
+	// const USERAGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:13.0) Gecko/20100101 Firefox/13.0';
+	const USERAGENT = 'ZPush ';
 
 	/**
 	 * CardDAV server url
